a5020d4e0a0ac7199f8eecaa7051350c81dde3f9
snigo <snigo.ogins@gmail.com>
2021-03-19 13:55:14 +0100

    set up next js boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,49 @@
 module.exports = {
   env: {
-    commonjs: true,
-    es6: true,
-    node: true,
     browser: true,
+    es2021: true,
+    node: true,
   },
   extends: [
-    'airbnb-base',
+    'plugin:react/recommended',
+    'airbnb',
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/react-in-jsx-scope': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/prop-types': 0,
+    'no-unused-vars': 0,
+    'no-unused-expressions': 0,
+    'no-confusing-arrow': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
 };
---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+const Index = () => (
+  <main>
+    <h1>ℵ℮ґⅾ¥ ℚυ◎тℯ$!</h1>
+    <div className="box-border">
+      <div className="overlay" />
+      <div className="box">
+        <div className="code">
+          <p className="code-line">
+            <span id="messageBox" />
+            <span className="cursor-blink">▋</span>
+          </p>
+        </div>
+      </div>
+      <button type="button" className="kbd-btn" id="nextBtn">NEXT</button>
+    </div>
+  </main>
+);
+
+export default Index;
